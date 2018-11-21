361a01de5aa55029cc99f91dd66ed67a9f31457d
Carlos Lantigua <clantigua954@gmail.com>
2018-11-21 12:21:20 -0500

    added nav and styling

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2aab5fa46d279ea9ad9d";
+/******/ 	var hotCurrentHash = "59207c2ceddd2eafb552";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
