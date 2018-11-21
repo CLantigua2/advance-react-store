8e491d4e4233ee315dd39dd55aebcef59088b90c
Carlos Lantigua <clantigua954@gmail.com>
2018-11-21 13:41:35 -0500

    completed Next server stylesheet

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "59207c2ceddd2eafb552";
+/******/ 	var hotCurrentHash = "08998b56987998343469";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
